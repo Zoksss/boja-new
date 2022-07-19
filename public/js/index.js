eddcd8234acb4aad6f8c8c419a04fc87d1b2820a
Zoksss <zorantodorovic80@gmail.com>
2022-07-19 23:46:42 +0200

    vesti, responsive fixes, nav animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+const navHamburger = document.querySelector("#checkbox_toggle");
+const menu = document.querySelector("#menu");
+
+navHamburger.addEventListener("click", () => {
+    console.log(navHamburger.checked);
+    if (!navHamburger.checked) {
+        menu.classList.add("navReverse");
+    }
+    setTimeout(() => {
+        menu.classList.remove("navReverse");
+        menu.style.display = "none";
+    }, 500);
+})
